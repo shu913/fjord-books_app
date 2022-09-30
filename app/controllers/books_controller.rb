aef0d0b01540e9ac3f7064c5c59565c72f2af763
shu913 <sss91327@icloud.com>
2022-10-01 00:22:46 +0900

    モデルごとに1ページあたりの表示件数を指定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.order(:create_at, :id).page(params[:page]).per(2)
+    @books = Book.order(:created_at, :id).page(params[:page])
   end
 
   # GET /books/1
