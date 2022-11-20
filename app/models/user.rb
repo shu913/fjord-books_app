2a40cfca0cfe51811b77e51eec7e49e80457e5b2
shu913 <sss91327@icloud.com>
2022-11-20 11:26:49 +0900

    フォロー機能 関連の記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_follows, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy, inverse_of: :follower
+  has_many :passive_follows, class_name: 'Follow', foreign_key: :followed_id, dependent: :destroy, inverse_of: :followed
+
+  has_many :followings, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
+
+  def follow(user_id)
+    active_follows.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    active_follows.find_by(followed_id: user_id).destroy
+  end
+
+  def followings?(user)
+    followings.include?(user)
+  end
 end
