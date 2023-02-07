ab55df358f45968e8767179ef3a31654ad78c8ab
shu913 <sss91327@icloud.com>
2023-02-07 20:51:11 +0900

    create・destroyメソッドの成功/失敗時の挙動に合わせてフラッシュメッセージを表示

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,11 +1,19 @@
 class FollowsController < ApplicationController
   def create
-    current_user.follow(params[:id])
-    redirect_to user_path
+    @user = current_user.follow(params[:id])
+    if @user.save
+      redirect_to user_path, notice: 'ユーザーのフォローに成功しました。'
+    else
+      redirect_to user_path, alert: 'ユーザーのフォローに失敗しました。'
+    end
   end
 
   def destroy
-    current_user.unfollow(params[:id])
-    redirect_to user_path
+    @user = current_user.unfollow(params[:id])
+    if @user
+      redirect_to user_path, notice: 'ユーザーのフォロー解除に成功しました。'
+    else
+      redirect_to user_path, alert: 'ユーザーのフォロー解除に失敗しました。'
+    end
   end
 end
