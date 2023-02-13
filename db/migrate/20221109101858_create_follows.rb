ae1da411638fb6860e1ea7d5fc14096bbe21488b
shu913 <sss91327@icloud.com>
2023-02-13 15:43:01 +0900

    インデント

--- db/migrate/20221109101858_create_follows.rb
+++ db/migrate/20221109101858_create_follows.rb
@@ -6,8 +6,8 @@ class CreateFollows < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
-  add_index :follows, :follower_id
-  add_index :follows, :followed_id
-  add_index :follows, [:follower_id, :followed_id], unique: true
+      add_index :follows, :follower_id
+      add_index :follows, :followed_id
+      add_index :follows, [:follower_id, :followed_id], unique: true
   end
 end
